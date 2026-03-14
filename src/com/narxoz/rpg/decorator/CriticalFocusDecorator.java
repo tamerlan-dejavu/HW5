869de5650fb4a5f8cc994588c46559e6a09f9433
tamerlan-dejavu <tamerlan.dejavu@gmail.com>
2026-03-14 22:42:42 +0500

    I replaced all md files to "hints" folder and created a foundation of decorator with basic logic

--- src/com/narxoz/rpg/decorator/CriticalFocusDecorator.java
+++ src/com/narxoz/rpg/decorator/CriticalFocusDecorator.java
@@ -7,19 +7,16 @@ public class CriticalFocusDecorator extends ActionDecorator {
 
     @Override
     public String getActionName() {
-        // TODO: Decide how this decorator changes the visible action name.
-        return super.getActionName();
+        return super.getActionName() + " (Critical)";
     }
 
     @Override
     public int getDamage() {
-        // TODO: Add critical-related behavior on top of wrapped damage.
-        return super.getDamage();
+        return (int) (super.getDamage() * 1.5);
     }
 
     @Override
     public String getEffectSummary() {
-        // TODO: Append or compose the critical effect description.
-        return super.getEffectSummary();
+        return super.getEffectSummary() + " [x1.5 multiplier]";
     }
 }
