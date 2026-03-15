b3bb22eb0ff5d2e1c5e7a0d3117a144388d44a39
tamerlan-dejavu <tamerlan.dejavu@gmail.com>
2026-03-15 13:54:16 +0500

    I have done all TODO in enemy class (extended logic)

--- src/com/narxoz/rpg/enemy/BossEnemy.java
+++ src/com/narxoz/rpg/enemy/BossEnemy.java
@@ -24,12 +24,10 @@ public class BossEnemy {
     }
 
     public void takeDamage(int amount) {
-        // TODO: Decide how boss damage should be applied and clamped.
-        health -= amount;
+        this.health = Math.max(0, this.health - amount);
     }
 
     public boolean isAlive() {
-        // TODO: Decide whether additional boss states belong here.
-        return health > 0;
+        return this.health > 0;
     }
 }
