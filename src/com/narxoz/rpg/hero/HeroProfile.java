6a04736123ffaa51097569f2cf74c187b58620a0
tamerlan-dejavu <tamerlan.dejavu@gmail.com>
2026-03-15 13:32:02 +0500

    I have done all TODO in hero class (extended logic)

--- src/com/narxoz/rpg/hero/HeroProfile.java
+++ src/com/narxoz/rpg/hero/HeroProfile.java
@@ -18,12 +18,10 @@ public class HeroProfile {
     }
 
     public void takeDamage(int amount) {
-        // TODO: Decide how health should be reduced and clamped.
-        health -= amount;
+        this.health = Math.max(0, this.health - amount);
     }
 
     public boolean isAlive() {
-        // TODO: Decide whether additional conditions belong here.
-        return health > 0;
+        return this.health > 0;
     }
 }
